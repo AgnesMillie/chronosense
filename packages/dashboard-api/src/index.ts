74db5aaa140b1bdc28aa1975db73dc3dff2517cb
violetcannary <daycaremoon22@gmail.com>
2025-10-01 13:42:11 -0300

    feat(dashboard-api): setup initial express server

---
+++ packages/dashboard-api/src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response, Application } from 'express';
+
+const app: Application = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({ message: 'Chronosense Dashboard API is running!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
